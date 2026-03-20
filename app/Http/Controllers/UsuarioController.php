9bc33b50b495ae12d01cbb28a374c0d73eb3ae91
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2026-03-19 23:29:01 -0400

    [ADD] - controller config

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,47 @@
+
+<?
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioController extends Controller
+{
+
+
+    public function cadastro(Request $request)
+    {
+        $request->validate([
+            'nome'  => 'required|string|min:3',
+            'email' => 'required|email|unique:usuarios',
+            'senha' => 'required|min:6'
+        ]);
+
+        Usuario::create([
+            'nome'  => $request->nome,
+            'email' => $request->email,
+            'senha' => Hash::make($request->senha),
+        ]);
+
+        return response()->json(['message' => 'Usuário cadastrado com sucesso!'], 201);
+    }
+
+    public function atualizar(Request $request, $id)
+    {
+        $usuario = Usuario::findOrFail($id);
+
+        $request->validate([
+            'senha' => 'sometimes|required|min:6'
+        ]);
+
+        if ($request->has('senha')) {
+            $usuario->senha = Hash::make($request->senha);
+        }
+
+        $usuario->save();
+
+        return response()->json(['message' => 'Senha atualizado com sucesso!']);
+    }
+}
