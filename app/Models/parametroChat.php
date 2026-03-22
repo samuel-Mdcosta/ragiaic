9ed17dbf8cd6def6c77f7b43fefcfbc7e18991f8
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2026-03-22 11:42:20 -0400

    [ADD] - db model chat parameters

---
+++ app/Models/parametroChat.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class parametroChat extends Model
+{
+
+    protected $table = 'parametros_chat';
+
+    protected $fillable = [
+        'usuario_id',
+        'usoChat',
+        'tempoUsoChat'
+    ];
+}
