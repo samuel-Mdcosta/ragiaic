5f2f847a51ef28217745f1aa612dc6c7b87e000e
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2026-04-13 22:04:56 -0400

    [FIX] - front request

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(remove: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+        $middleware->api(prepend: [
+            \App\Http\Middleware\ForceJsonResponse::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
